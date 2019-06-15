b8dde0ccbb6849052cf0cbff672ec127ab2fa5ce
ashu8912 <ashughildiyal5@gmail.com>
2019-06-15 17:56:19 +0530

    adjusted code

--- enhancedRockPaperScissor.py
+++ enhancedRockPaperScissor.py
@@ -9,23 +9,23 @@ elif(rand_num==1):
 else:
 	ai_hit="scissor";
 
-player1_hit=input("player choose your hit");
+player1_hit=input("player choose your hit\n").lower();
 if(player1_hit==ai_hit):
 	print("This is a tie");
 elif(player1_hit=="rock"):
 	if(ai_hit=="paper"):
 		print("ai choose paper and it won");
-	elif(ai_hit=="scissor"):
+	else:
 		print("ai choose scissor and it lost,You WON");
 elif(player1_hit=="paper"):
  	if(ai_hit=="scissor"):
  		print("ai choose scissor and you lost");
- 	elif(ai_hit=="rock"):
+ 	else:
  		print("ai choose rock and it lost,YOU WON");
 elif(player1_hit=="scissor"):
 	if(ai_hit=="rock"):
 		print("ai choose rock and it won");
-	elif(ai_hit=="paper"):
+	else:
 		print("ai choose paper and it lost,YOU WON");	
 else:
 	print("something went wrong");			 							  		
