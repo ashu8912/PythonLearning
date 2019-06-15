f8be4eefa7f9f8d841303a3a0d437ac0f60303b2
ashu8912 <ashughildiyal5@gmail.com>
2019-06-15 17:49:08 +0530

    added randomness to rock paper scissor

---
+++ enhancedRockPaperScissor.py
@@ -0,0 +1,31 @@
+import random;
+
+rand_num=random.randint(0,2);
+
+if(rand_num==0):
+	ai_hit="rock";
+elif(rand_num==1):
+  	ai_hit="paper";
+else:
+	ai_hit="scissor";
+
+player1_hit=input("player choose your hit");
+if(player1_hit==ai_hit):
+	print("This is a tie");
+elif(player1_hit=="rock"):
+	if(ai_hit=="paper"):
+		print("ai choose paper and it won");
+	elif(ai_hit=="scissor"):
+		print("ai choose scissor and it lost,You WON");
+elif(player1_hit=="paper"):
+ 	if(ai_hit=="scissor"):
+ 		print("ai choose scissor and you lost");
+ 	elif(ai_hit=="rock"):
+ 		print("ai choose rock and it lost,YOU WON");
+elif(player1_hit=="scissor"):
+	if(ai_hit=="rock"):
+		print("ai choose rock and it won");
+	elif(ai_hit=="paper"):
+		print("ai choose paper and it lost,YOU WON");	
+else:
+	print("something went wrong");			 							  		
