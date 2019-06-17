dbcf1bbd1236ecd27aabd5367bbd36e4ab4773a2
ashu8912 <ashughildiyal5@gmail.com>
2019-06-17 09:35:49 +0530

    python dictionary playlist samples

---
+++ playlist.py
@@ -0,0 +1,16 @@
+playlist={
+  "title":"Fun Songs",
+  "author":"ashu",
+  "songs":[{
+    "title":"kalank",
+    "artist":"arijit",
+    "duration":2.5
+  },{
+    "title":"love you zindagi",
+    "artist":"amit trivedi",
+    "duration":3.5
+  },{
+    "title":"pashmeena dhaage",
+    "artist":"amit trivedi",
+    "duration":4
+  }]
